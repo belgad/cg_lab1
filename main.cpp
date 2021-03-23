995d4c4894aa84048a247120d3dcce588778b264
Gleb Danshin <glebd79@gmail.com>
2021-03-23 11:53:18 +0300

    Minor updates

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
     int mathMorphologyKernelSize = 0;
     Kernel mathMorphologyKernel;
 
-    mathMorphologyKernelPath = "images/mathMorphologyKernel"; mathMorphology = true;
+//    mathMorphologyKernelPath = "images/mathMorphologyKernel"; mathMorphology = true;
 
     QImage img;
 
@@ -69,90 +69,90 @@ int main(int argc, char *argv[]) {
         temp.reset();
     }
 
-//    InvertFilter invert;
-//    invert.process(img).save("images/invert.png");
+    InvertFilter invert;
+    invert.process(img).save("images/invert.png");
 
-//    BlurFilter blur;
-//    blur.process(img).save("images/blur.png");
+    BlurFilter blur;
+    blur.process(img).save("images/blur.png");
 
-//    GaussianFilter gauss;
-//    gauss.process(img).save("images/gauss.png");
+    GaussianFilter gauss;
+    gauss.process(img).save("images/gauss.png");
 
-//    GrayScaleFilter grayScale;
-//    grayScale.process(img).save("images/grayScale.png");
+    GrayScaleFilter grayScale;
+    grayScale.process(img).save("images/grayScale.png");
 
-//    SepiaFilter sepia;
-//    sepia.process(img).save("images/sepia.png");
+    SepiaFilter sepia;
+    sepia.process(img).save("images/sepia.png");
 
-//    BrightnessFilter brightness;
-//    brightness.process(img).save("images/brightness.png");
+    BrightnessFilter brightness;
+    brightness.process(img).save("images/brightness.png");
 
-//    SobelFilterX sobelX;
-//    sobelX.process(img).save("images/sobelX.png");
+    SobelFilterX sobelX;
+    sobelX.process(img).save("images/sobelX.png");
 
-//    SobelFilterY sobelY;
-//    sobelY.process(img).save("images/sobelY.png");
+    SobelFilterY sobelY;
+    sobelY.process(img).save("images/sobelY.png");
 
-//    SharpnessFilter sharpness;
-//    sharpness.process(img).save("images/sharpness.png");
+    SharpnessFilter sharpness;
+    sharpness.process(img).save("images/sharpness.png");
 
-//    GrayWorldFilter grayWorld;
-//    grayWorld.process(img).save("images/grayWorld.png");
+    GrayWorldFilter grayWorld;
+    grayWorld.process(img).save("images/grayWorld.png");
 
-//    PerfectReflectorFilter perfectReflector;
-//    perfectReflector.process(img).save("images/perfectReflector.png");
+    PerfectReflectorFilter perfectReflector;
+    perfectReflector.process(img).save("images/perfectReflector.png");
 
-//    LinearHistogramChange linearHistogramChange;
-//    linearHistogramChange.process(img).save("images/linearHistogramChange.png");
+    HistogramLinearChange histogramLinearChange;
+    histogramLinearChange.process(img).save("images/histogramLinearChange.png");
 
-//    SobelFilter sobel;
-//    sobel.process(img).save("images/sobel.png");
+    SobelFilter sobel;
+    sobel.process(img).save("images/sobel.png");
 
-//    ScharrFilter scharr;
-//    scharr.process(img).save("images/scharr.png");
+    ScharrFilter scharr;
+    scharr.process(img).save("images/scharr.png");
 
-//    PrewittFilter prewitt;
-//    prewitt.process(img).save("images/prewitt.png");
+    PrewittFilter prewitt;
+    prewitt.process(img).save("images/prewitt.png");
 
-//    Sharpness2Filter sharpness2;
-//    sharpness2.process(img).save("images/sharpness2.png");
+    Sharpness2Filter sharpness2;
+    sharpness2.process(img).save("images/sharpness2.png");
 
-//    Dilation dilation(mathMorphologyKernel);
-//    dilation.process(img).save("images/dilation.png");
+    Dilation dilation(mathMorphologyKernel);
+    dilation.process(img).save("images/dilation.png");
 
-//    Erosion erosion(mathMorphologyKernel);
-//    erosion.process(img).save("images/erosion.png");
+    Erosion erosion(mathMorphologyKernel);
+    erosion.process(img).save("images/erosion.png");
 
-//    Opening opening(mathMorphologyKernel);
-//    opening.process(img).save("images/opening.png");
+    Opening opening(mathMorphologyKernel);
+    opening.process(img).save("images/opening.png");
 
-//    Closing closing(mathMorphologyKernel);
-//    closing.process(img).save("images/closing.png");
+    Closing closing(mathMorphologyKernel);
+    closing.process(img).save("images/closing.png");
 
-//    MorphologicalGradient morphGrad(mathMorphologyKernel);
-//    morphGrad.process(img).save("images/morphGrad.png");
+    MorphologicalGradient morphGrad(mathMorphologyKernel);
+    morphGrad.process(img).save("images/morphGrad.png");
 
-//    MorphologicalTopHat morphTopHat(mathMorphologyKernel);
-//    morphTopHat.process(img).save("images/morphTopHat.png");
+    MorphologicalTopHat morphTopHat(mathMorphologyKernel);
+    morphTopHat.process(img).save("images/morphTopHat.png");
 
-//    MorphologicalBlackHat morphBlackHat(mathMorphologyKernel);
-//    morphBlackHat.process(img).save("images/morphBlackHat.png");
+    MorphologicalBlackHat morphBlackHat(mathMorphologyKernel);
+    morphBlackHat.process(img).save("images/morphBlackHat.png");
 
-//    MedianFilter median;
-//    median.process(img).save("images/median.png");
+    MedianFilter median;
+    median.process(img).save("images/median.png");
 
-//    BaseColorCorrection baseColor;
-//    baseColor.process(img).save("images/baseColor.png");
+    BaseColorCorrection baseColor(0.67f, 0.34f, 0.18f);
+    baseColor.process(img).save("images/baseColor.png");
 
-//    MoveFilter move;
-//    move.process(img, 50, 0).save("images/move.png");
+    MoveFilter move;
+    move.process(img, 50, 0).save("images/move.png");
 
-//    RotateFilter rotate;
-//    rotate.process(img, img.width() / 2, img.height() / 2, M_PI_4).save("images/rotate.png");
+    RotateFilter rotate;
+    rotate.process(img, img.width() / 2, img.height() / 2, M_PI_4).save("images/rotate.png");
 
-//    WavesFilter waves;
-//    waves.process(img, 60, 0).save("images/waves1.png");
-//    waves.process(img, 30, 1).save("images/waves2.png");
+    WavesFilter waves;
+    waves.process(img, 60, 0).save("images/waves1.png");
+    waves.process(img, 30, 1).save("images/waves2.png");
 
     GlassFilter glass;
     glass.process(img).save("images/glass.png");
